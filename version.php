3b5a9a1494c15e58f96e2634ad64d2b4aa7fa260
Sam Suresh <sam@mu.my>
2020-05-05 20:11:53 +0800

    vup

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020050500;
+$plugin->version   = 2020050501;
 $plugin->requires  = 2013110500;
 $plugin->component = 'local_cleanurls';
 
